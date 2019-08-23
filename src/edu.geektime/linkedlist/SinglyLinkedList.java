6e7999ba2ad01a3c6006afa71c2c8d77128ba722
ArchyShawn <archy.shawn@gmail.com>
2019-08-23 11:07:38 +0800

    feat: SinglyLinkedList

--- src/edu.geektime/linkedlist/SinglyLinkedList.java
+++ src/edu.geektime/linkedlist/SinglyLinkedList.java
@@ -22,7 +22,7 @@ public class SinglyLinkedList {
         int pos = 0;
         while (p != null && pos != index) {
             p = p.next;
-
+            ++pos;
         }
 
         return p;
@@ -41,4 +41,112 @@ public class SinglyLinkedList {
             return data;
         }
     }
+
+    // 无头结点
+    // 表头部插入
+    // 用于逆序链表
+    public void insertToHead(int value) {
+        Node newNode = new Node(value, null);
+        insertToHead(newNode);
+    }
+
+    public void insertToHead(Node newNode) {
+        if (head == null) {
+
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    // 顺序插入
+    // 链表尾部插入
+    public void insertTail(int value) {
+
+        Node newNode = new Node(value, null);
+        // 空链表，可以插入新节点作为head,也可以不操作
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node q = head;
+            while (q.next != null) {
+                q = q.next;
+            }
+            newNode.next = q.next;
+            q.next = newNode;
+        }
+    }
+
+    public void insertAfter(Node p, int value) {
+        Node newNode = new Node(value, null);
+        insertAfter(p, newNode);
+    }
+
+    public void insertAfter(Node p, Node newNode) {
+        if (p == null) {
+            return;
+        }
+
+        newNode.next = p.next;
+        p.next = newNode;
+    }
+
+    public void insertBefore(Node p, int value) {
+        Node newNode = new Node(value, null);
+        insertBefore(p, newNode);
+    }
+
+    public void insertBefore(Node p, Node newNode) {
+        if (p == null) {
+            return;
+        }
+        if (head == p) {
+            insertToHead(newNode);
+            return;
+        }
+
+        Node q = head;
+        while (q != null && q.next != p) {
+            q = q.next;
+        }
+
+        if (q == null) {
+            return;
+        }
+
+        newNode.next = p;
+        q.next = newNode;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
