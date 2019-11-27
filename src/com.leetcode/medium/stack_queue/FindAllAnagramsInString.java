af27c6e78272dde41c63cf5cd753cee18b600141
ArchyShawn <archy.shawn@gmail.com>
2019-11-27 20:05:13 +0800

    feat: stack & queue

--- src/com.leetcode/medium/stack_queue/FindAllAnagramsInString.java
+++ src/com.leetcode/medium/stack_queue/FindAllAnagramsInString.java
@@ -101,4 +101,39 @@ public class FindAllAnagramsInString {
         }
         return result;
     }
+
+    // Time: O(n), Space: O(k)
+    // 先用一个整型数组作为哈希表，对子串进行计数（取字符 p.charAt(i) 到 ‘a’ 的偏移作为下标）
+    // 定义滑动窗口左右游标，从左往右遍历字符串 s
+    // 若当前字符在哈希表中存在（pc[s.charAt(right) - 'a'] > 0）, 则抵消计数，且继续向右移动游标来扩大窗口
+    // 反之，说明不存在，将窗口左边界游标作为哈希表下标，对该位置 +1，且左边界游标右移来缩小窗口
+    // 经过上述一次判断操作后，计算左右游标间距是否等于子串长度（或者看哈希表内是否都为零，正好被窗口内字符串抵消）
+    // 若相等则记录当前左边界游标位置
+    public List<Integer> findAnagrams02(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || p == null || s.length() < p.length()) {
+            return result;
+        }
+        int pLen = p.length(), sLen = s.length();
+        int[] pc = new int[26];
+        for (int  i = 0; i < pLen; i++) {
+            pc[p.charAt(i) - 'a']++;
+        }
+
+        int left = 0, right = 0;
+        while (right < sLen) {
+            if (pc[s.charAt(right) - 'a'] > 0) {
+                pc[s.charAt(right) - 'a']--;
+                right++;
+            } else {
+                pc[s.charAt(left) - 'a']++;
+                left++;
+            }
+            if (right - left == pLen) {
+                result.add(left);
+            }
+        }
+        return result;
+    }
+
 }
