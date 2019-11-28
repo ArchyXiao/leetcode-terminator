13cd9e006fddc7731d31e8227a4589d784f8098e
ArchyShawn <archy.shawn@gmail.com>
2019-11-28 18:28:54 +0800

    feat: stack & queue

--- src/com.leetcode/hard/stack_queue/MinimumWindowSubstring.java
+++ src/com.leetcode/hard/stack_queue/MinimumWindowSubstring.java
@@ -18,4 +18,39 @@ package com.leetcode.hard.stack_queue;
  */
 public class MinimumWindowSubstring {
 
+    // Time: O(n), Space: O(m)
+    public String minWindow(String s, String t) {
+        if (s == null || t == null || s.length() < t.length()) {
+            return "";
+        }
+
+        int left = 0, right = 0;
+        int start = 0, len = Integer.MAX_VALUE, requireCnt = t.length();
+        int[] required = new int[256];
+        for (int i = 0; i < t.length(); i++) {
+            required[t.charAt(i)]++;
+        }
+
+        for (; right < s.length(); right++) {
+            char r = s.charAt(right);
+            if (required[r] > 0) {
+                requireCnt--;
+            }
+            required[r]--;
+
+            while (requireCnt == 0) {
+                if (right - left + 1 < len) {
+                    len = right - left + 1;
+                    start = left;
+                }
+                char l = s.charAt(left);
+                required[l]++;
+                if (required[l] > 0) {
+                    requireCnt++;
+                }
+                left++;
+            }
+        }
+        return len == Integer.MAX_VALUE ? "" : s.substring(start, start + len);
+    }
 }
