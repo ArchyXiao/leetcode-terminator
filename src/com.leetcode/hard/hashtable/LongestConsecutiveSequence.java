25d7951fcc9bd7ef46f240c179da3447e6005a76
archy <archy.shawn@gmail.com>
2019-12-03 23:48:15 +0800

    feat: hashtable

--- src/com.leetcode/hard/hashtable/LongestConsecutiveSequence.java
+++ src/com.leetcode/hard/hashtable/LongestConsecutiveSequence.java
@@ -1,5 +1,9 @@
 package hashtable;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @Description:
  * Given an unsorted array of integers, find the length of the longest consecutive elements sequence.
@@ -17,4 +21,53 @@ package hashtable;
  */
 public class LongestConsecutiveSequence {
 
+    // Time: O(n*log(n)), Space: O(1)
+    // 基于比较的排序，是主要的消耗点
+    // 排序后的数组是一个非递减序列，可以利用一个游标和一个额外字段，来完成最大递增子序列统计
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int max = 0, p = 0;
+        while (p < nums.length) {
+            int len = 1;
+            while (p < nums.length - 1) {
+                if (nums[p + 1] - nums[p] > 1) {
+                    break;
+                }
+                if (nums[p + 1] - nums[p] == 1) {
+                    len++;
+                }
+                p++;
+            }
+            max = Math.max(max, len);
+            p++;
+        }
+        return max;
+    }
+
+    // Time: O(n), Space: O(n)
+    public int longestConsecutive02(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+
+        int max = 0;
+        for (int  i = 0; i < nums.length && !set.isEmpty(); i++) {
+            int low = nums[i], high = nums[i];
+            while (set.contains(--low)) {
+                set.remove(low);
+            }
+            while (set.contains(++high)) {
+                set.remove(high);
+            }
+            max = Math.max(max, high - low - 1);
+        }
+        return max;
+    }
 }
